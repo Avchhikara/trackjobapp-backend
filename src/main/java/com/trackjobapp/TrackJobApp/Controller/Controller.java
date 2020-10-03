447a03468908bcae0044b0a280bdbe904e212ae3
Avnish <Avnishway@gmail.com>
2020-10-04 00:15:17 +0530

    Removing config

--- src/main/java/com/trackjobapp/TrackJobApp/Controller/Controller.java
+++ src/main/java/com/trackjobapp/TrackJobApp/Controller/Controller.java
@@ -1,16 +1,15 @@
 package com.trackjobapp.TrackJobApp.Controller;
 
-import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class Controller {
 
     @GetMapping
     public String Home() {
-        log.info("Serving homepage");
         return "<h1>Hello</h1>";
     }
 }
