fa89c6f54d6b218506b491baf668148f100f6c47
Avnish <Avnishway@gmail.com>
2020-10-03 23:32:32 +0530

    init

---
+++ src/main/java/com/trackjobapp/TrackJobApp/TrackJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.trackjobapp.TrackJobApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackJobAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackjobapp/TrackJobApp/TrackJobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackjobapp.TrackJobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackJobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
